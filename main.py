e2e2cd1775f539388fdce4187564432a9659504b
Norvik-Alexian <norvik.alexyan97@protonmail.com>
2023-04-29 21:44:02 +0400

    add more visualization to the code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 import glob
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -120,13 +121,6 @@ month_map = {
 }
 
 def parse_clf_time(text):
-    """ Convert Common Log time format into a Python datetime object
-    Args:
-        text (str): date and time in Apache time format [dd/mmm/yyyy:hh:mm:ss (+/-)zzzz]
-    Returns:
-        a string suitable for passing to CAST('timestamp')
-    """
-    # NOTE: We're ignoring the time zones here, might need to be handled depending on the problem you are solving
     return "{0:04d}-{1:02d}-{2:02d} {3:02d}:{4:02d}:{5:02d}".format(
       int(text[7:11]),
       month_map[text[3:6]],
@@ -160,7 +154,69 @@ status_freq_df = (logs_df.groupBy('status').count().sort('status').cache())
 # print('Total distinct HTTP Status Codes:', status_freq_df.count())
 
 status_freq_pd_df = (status_freq_df.toPandas().sort_values(by=['count'],ascending=False))
-# print(status_freq_pd_df)
-
 sns.catplot(x='status', y='count', data=status_freq_pd_df, kind='bar', order=status_freq_pd_df['status'])
-plt.show()
+
+log_freq_df = status_freq_df.withColumn('log(count)', F.log(status_freq_df['count']))
+# log_freq_df.show()
+
+log_freq_pd_df = (log_freq_df.toPandas().sort_values(by=['log(count)'],ascending=False))
+sns.catplot(x='status', y='log(count)', data=log_freq_pd_df, kind='bar', order=status_freq_pd_df['status'])
+
+# Analyzing Frequent Hosts
+host_sum_df =(logs_df.groupBy('host').count().sort('count', ascending=False).limit(10))
+# host_sum_df.show(truncate=False)
+
+# host_sum_pd_df = host_sum_df.toPandas()
+# print(host_sum_pd_df.iloc[8]['host'])
+
+# Display the Top 20 Frequent EndPoints
+paths_df = (logs_df.groupBy('endpoint').count().sort('count', ascending=False).limit(20))
+paths_pd_df = paths_df.toPandas()
+
+# Top Ten Error Endpoints
+not200_df = (logs_df.filter(logs_df['status'] != 200))
+error_endpoints_freq_df = (not200_df.groupBy('endpoint').count().sort('count', ascending=False).limit(10))
+# error_endpoints_freq_df.show(truncate=False)
+
+# Average Number of Daily Requests per Host
+host_day_df = logs_df.select(logs_df.host, F.dayofmonth('time').alias('day'))
+host_day_distinct_df = (host_day_df.dropDuplicates())
+
+# host_day_df.show(5, truncate=False)
+# host_day_distinct_df.show(5, truncate=False)
+
+daily_hosts_df = (host_day_distinct_df.groupBy('day').count().select(col("day"),col("count").alias("total_hosts")))
+total_daily_reqests_df = (logs_df.select(F.dayofmonth("time").alias("day")).groupBy("day").count().select(col("day"), col("count").alias("total_reqs")))
+
+avg_daily_reqests_per_host_df = total_daily_reqests_df.join(daily_hosts_df, 'day')
+avg_daily_reqests_per_host_df = (avg_daily_reqests_per_host_df.withColumn('avg_reqs', col('total_reqs') / col('total_hosts')).sort("day"))
+avg_daily_reqests_per_host_df = avg_daily_reqests_per_host_df.toPandas()
+
+avg_daily_request_plot = sns.catplot(x='day', y='avg_reqs', data=avg_daily_reqests_per_host_df, kind='point', height=5, aspect=1.5)
+
+# Counting 404 Response Codes
+not_found_df = logs_df.filter(logs_df["status"] == 404).cache()
+total_404_response = ('Total 404 responses: {}').format(not_found_df.count())
+endpoints_404_count_df = (not_found_df.groupBy("endpoint").count().sort("count", ascending=False).limit(20))
+# endpoints_404_count_df.show(truncate=False)
+
+# Listing the Top Twenty 404 Response Code Hosts
+hosts_404_count_df = (not_found_df.groupBy("host").count().sort("count", ascending=False).limit(20))
+# hosts_404_count_df.show(truncate=False)
+
+# Visualizing 404 Errors per Day
+errors_by_date_sorted_df = (not_found_df.groupBy(F.dayofmonth('time').alias('day')).count().sort("day"))
+errors_by_date_sorted_pd_df = errors_by_date_sorted_df.toPandas()
+
+# visualizing 404 erros per day
+response_404_plot = sns.catplot(x='day', y='count', data=errors_by_date_sorted_pd_df, kind='point', height=5, aspect=1.5)
+
+# Top Three Days for 404 Errors
+top_three_404_response = (errors_by_date_sorted_df.sort("count", ascending=False).show(3))
+
+# Visualizing Hourly 404 Errors
+hourly_avg_errors_sorted_df = (not_found_df.groupBy(F.hour('time').alias('hour')).count().sort('hour'))
+hourly_avg_errors_sorted_pd_df = hourly_avg_errors_sorted_df.toPandas()
+
+hourly_404_plot = sns.catplot(x='hour', y='count', data=hourly_avg_errors_sorted_pd_df, kind='bar', height=5, aspect=1.5)
+# plt.show()
